0560d7cce8f9cd8563fc0b00950595476f6c2c29
irregular-user <heladio522@gmail.com>
2026-06-06 01:12:58 -0700

    just more code

--- autoperception/main.cpp
+++ autoperception/main.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 
 int main(){
+    Vec2 origin;
+
+    Vec2 point1(3.0,4.0);
+
+    cout << "Point P = ";
+    point1.print();
+    cout << endl;
+
+    cout << "Magnitdue of p = " << point1.magnitude() << endl;
+    cout << "Distance from origin to point 1: " << origin.distanceTo(point1) << endl;
 
 
     return 0;
